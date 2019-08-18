917e5eb72ca30db8fcff1ef7ca437bbf71f6fb73
marquet <marquet@element84.com>
2019-08-17 22:21:08 -0400

    set up initial rules and script for linter

---
+++ config/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			1
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			2,
+			'single',
+			'avoid-escape'
+		],
+		'semi': [
+			1,
+			'never'
+		]
+	}
+}
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,74 +2,74 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Button = ({
-  children = 'Button', 
-  className, 
-  href,
-  onClick, 
-  disabled = false, 
-  type, 
-  ...props 
+ children = 'Button', 
+ className, 
+ href,
+ onClick, 
+ disabled = false, 
+ type, 
+ ...props 
 } = {}) => {
-  let buttonTag = null
+ let buttonTag = null
 
-  className = `${className} button` || 'button'
+ className = `${className} button` || 'button'
   
-  const attributes = {
-    className,
-    children,
-    disabled,
-    onClick,
-    href
-  };
+ const attributes = {
+  className,
+  children,
+  disabled,
+  onClick,
+  href
+ }
 
-  if(disabled) {
-    className += ' disabled-button'
-    disabled = true
+ if(disabled) {
+  className += ' disabled-button'
+  disabled = true
+ }
+
+ if(type) {
+  if(typeof type === 'string') {
+   type = [type]
   }
+  type.forEach(t => {
+   className += ` ${t}-button`
+  })
+ }
 
-  if(type) {
-    if(typeof type === 'string') {
-      type = [type]
-    }
-    type.forEach(t => {
-      className += ` ${t}-button`
-    })
-  }
-
-  if(!href || disabled) {
-    buttonTag = (
-      <button {...attributes} {...props}>
-        {children}
-      </button>
-    )
-  } else {
-    buttonTag = (
-      <button {...attributes} {...props}>
-        {children}
-      </button>
-    )
-  }
-
-  return (
-    <span className={className}>
-      {buttonTag}
-    </span>
+ if(!href || disabled) {
+  buttonTag = (
+   <button {...attributes} {...props}>
+    {children}
+   </button>
   )
+ } else {
+  buttonTag = (
+   <button {...attributes} {...props}>
+    {children}
+   </button>
+  )
+ }
+
+ return (
+  <span className={className}>
+   {buttonTag}
+  </span>
+ )
 }
 
 Button.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]),
-  type: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.string),
-    PropTypes.string
-  ]),
-  disable: PropTypes.bool,
-  type: PropTypes.string,
-  className: PropTypes.string,
-  onClick: PropTypes.func
+ children: PropTypes.oneOfType([
+  PropTypes.arrayOf(PropTypes.node),
+  PropTypes.node
+ ]),
+ type: PropTypes.oneOfType([
+  PropTypes.arrayOf(PropTypes.string),
+  PropTypes.string
+ ]),
+ disable: PropTypes.bool,
+ type: PropTypes.string,
+ className: PropTypes.string,
+ onClick: PropTypes.func
 }
 
 export default Button
