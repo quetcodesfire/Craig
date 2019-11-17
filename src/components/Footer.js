69b2da0c19bf78c4ec787b51eeffe686447f806e
marquet <marquet@element84.com>
2019-11-17 12:17:37 -0500

    preparing for new repurpose of CRAIG

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => (
- <footer className="footer">
-  <p>This is the footer</p>
- </footer>
-)
-
-export default Footer
--- src/stories/components/Button.stories.js
+++ src/stories/components/Button.stories.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
+// import React from 'react'
+// import { storiesOf } from '@storybook/react'
 
-import {Button} from '../../components/Button'
+// import {Button} from '../../components/Button'
 
-const stories = storiesOf('Components|Button', module)
+// const stories = storiesOf('Components|Button', module)
 
-stories.add('Default', () => {
- return (
-    <>
-      <p>
-       <Button className="test">Button No Link</Button>
-      </p>
-      <p>
-       <Button href="/" className="test">Button With Link</Button>
-      </p>
-      <p>
-       <Button className="test" type="circle" href="/"></Button>
-      </p>
-    </>
- )
-})
+// stories.add('Default', () => {
+//  return (
+//     <>
+//       <p>
+//        <Button className="test">Button No Link</Button>
+//       </p>
+//       <p>
+//        <Button href="/" className="test">Button With Link</Button>
+//       </p>
+//       <p>
+//        <Button className="test" type="circle" href="/"></Button>
+//       </p>
+//     </>
+//  )
+// })
 
-stories.add('Disabled', () => {
- return (
-    <>
-      <p>
-       <Button className="test" disabled={true}>Button No Link</Button>
-      </p>
-      <p>
-       <Button className="test" href="/" disabled={true} >Button With Link</Button>
-      </p>
-      <p>
-       <Button className="test" type="circle" href="/" disabled={true}></Button>
-      </p>
-    </>
- )
-})
+// stories.add('Disabled', () => {
+//  return (
+//     <>
+//       <p>
+//        <Button className="test" disabled={true}>Button No Link</Button>
+//       </p>
+//       <p>
+//        <Button className="test" href="/" disabled={true} >Button With Link</Button>
+//       </p>
+//       <p>
+//        <Button className="test" type="circle" href="/" disabled={true}></Button>
+//       </p>
+//     </>
+//  )
+// })
 
-//TODO: add buttons with icons
+// //TODO: add buttons with icons
